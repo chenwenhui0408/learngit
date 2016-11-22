193df86eddbacdc27c6c309cb51fc48548c5d8e8
nismo <12275114@qq.com>
2016-11-22 09:08:53 +0800

    second2 commit

--- app/src/main/java/com/b/a/myapplication/MainActivity.java
+++ app/src/main/java/com/b/a/myapplication/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
+        //mamme
         return super.onOptionsItemSelected(item);
     }
 }
